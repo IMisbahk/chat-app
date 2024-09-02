f455ebbc91f4ad8f221c0d72dcd1e523b04b5b57
IMisbahK <m.misbahkhursheed@gmail.com>
2024-09-02 22:39:50 +0530

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import Box from './components/chatarea'
 import { Chat } from './components/chatarea'
-
+import './App.css'
 export default function App(){
     return(
         <>
         <Box>
-        <Chat></Chat>
-        <Chat></Chat>
-        <Chat></Chat>
-        <Chat></Chat>
+            <Chat></Chat>
+            <Chat></Chat>
+            <Chat></Chat>
+            <Chat></Chat>
         </Box>
         </>
     )
--- src/components/chatarea.jsx
+++ src/components/chatarea.jsx
@@ -1,14 +1,19 @@
-import './App.css'
+import './src/App.css'
+
 export function Chat(){
     return(
         <div className="chat">
             <div className="pfp">
-
+                
             </div>
         </div>
     )
 }
 
 export default function Box(){
+    return(
+        <div className="chatarea">
 
+        </div>
+    )
 }
